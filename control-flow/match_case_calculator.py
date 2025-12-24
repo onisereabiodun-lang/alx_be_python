23bd2e10a2b06dc1a9732e1de346b2fc51a74ccf
onisereabiodun@gmail.com <ayodejiadejuwon@ayodejis-MacBook-Pro.local>
2025-12-24 07:52:35 +0100

    Add match_case_calculator.py using match-case statement

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the scond number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is result {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is result {result}.")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+    case _:
+        print("Invalid operation.")
+                               
