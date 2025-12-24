eb52acd8a7b394a05fe495986448e85505dea55b
onisereabiodun@gmail.com <ayodejiadejuwon@ayodejis-MacBook-Pro.local>
2025-12-24 15:00:29 +0100

    Add multiplication_table.py using for loop

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number to see it's multiplication table: "))
+for multiplicfor multiplier in range(1, 11):
+result = number * multiplier
+print(f"{number} * {multiplier} = {result}")
